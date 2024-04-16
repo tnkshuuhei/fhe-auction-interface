bde1d0703ff0efbb928e615511956923eac395f0
shuhei tanaka <shuhei46491@gmail.com>
2024-04-16 22:14:43 +0900

    add custom zamaDevnet

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,20 +5,30 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider, http } from "wagmi";
 import { optimismSepolia } from "wagmi/chains";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { defineChain } from "viem";
 
 import React from "react";
 import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/toaster";
 
+const zamaDevnet = defineChain({
+  id: 8009,
+  name: "Zama Network",
+  nativeCurrency: { name: "ZAMA", symbol: "ZAMA", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://devnet.zama.ai"] },
+  },
+  blockExplorers: {
+    default: { name: "Default Explore", url: "https://main.explorer.zama.ai" },
+  },
+});
+
 const config = getDefaultConfig({
   appName: "Scaffold",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-  chains: [optimismSepolia],
+  chains: [zamaDevnet],
   transports: {
-    [optimismSepolia.id]: http(
-      `https://optimism-sepolia.infura.io/v3/${process.env
-        .NEXT_PUBLIC_INFURA_API_KEY!}`
-    ),
+    [zamaDevnet.id]: http("https://devnet.zama.ai"),
   },
   ssr: true,
 });
--- components/Header.tsx
+++ components/Header.tsx
@@ -32,14 +32,14 @@ const Header = () => {
                   Home
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   href="/user"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                 >
                   Account
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <a
                   href="https://github.com/tnkshuuhei"

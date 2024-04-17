ab632234dd4cd44d67dac951a0df68a319d9faea
shuhei tanaka <shuhei46491@gmail.com>
2024-04-17 22:02:37 +0900

    fix precompile

--- contexts/index.tsx
+++ contexts/index.tsx
@@ -21,10 +21,12 @@ export const StateProvider = ({ children }: { children: React.ReactNode }) => {
   }, [provider, address]);
   const createFhevmInstance = async () => {
     const publicKey = await provider?.call({
-      from: null,
-      to: "0x0000000000000000000000000000000000000044",
+      // fhe lib address, may need to be changed depending on network
+      to: "0x000000000000000000000000000000000000005d",
+      // first four bytes of keccak256('fhePubKey(bytes1)') + 1 byte for library
+      data: "0xd9d47bb001",
     });
-    console.log("publicKey", publicKey);
+
     const instance = await createInstance({ chainId, publicKey } as {
       chainId: number;
       publicKey: string;

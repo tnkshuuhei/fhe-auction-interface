68f9c5a146533b64c224fde86ef3a38a9bc98658
shuhei tanaka <shuhei46491@gmail.com>
2024-04-18 20:47:20 +0900

    add sample auction page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,21 @@ import { Button } from "@/components/ui/button";
 import { StateContext } from "@/contexts";
 import { FhevmInstance } from "fhevmjs";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React, { useState } from "react";
+
 export default function Home() {
   const { getInstance, isInitialized } = StateContext();
+  const [amount, setAmount] = useState<string>("");
 
   const instance: FhevmInstance = getInstance();
 
@@ -15,9 +28,55 @@ export default function Home() {
     console.log("euint32: ", euint32);
   }
 
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const regex = /^[0-9]*[.,]?[0-9]*$/;
+    if (regex.test(value) || value === "") {
+      setAmount(value);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={() => encryptEuint32()}>Click me</Button>
+    <main>
+      {/* <Button onClick={() => encryptEuint32()}>Click me</Button> */}
+      <Card className="lg:w-[1000px] w-[350px] mx-auto my-12">
+        <CardHeader>
+          <CardTitle>Vickrey Auction</CardTitle>
+          <CardDescription>
+            This auction is fully homomorphic encrypted using Fhevm by zama.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="lg:grid lg:grid-cols-2 gap-10">
+            <div className="mx-auto">
+              <Image
+                src="https://picsum.photos/300/300"
+                width={300}
+                height={300}
+                alt="auction content"
+              />
+            </div>
+            <div className="flex flex-col gap-4 ">
+              <Label className=" font-semibold text-xl">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              </Label>
+              <Label className="text-gray-400">End: </Label>
+              <Label>Place your bid</Label>
+              <Input
+                type="text"
+                placeholder="Enter your bid"
+                className="w-full"
+                value={amount}
+                pattern="^[0-9]*[.,]?[0-9]*$" // only allow numbers
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleInput(e)
+                }
+              />
+              <Button className="w-full mt-4">Place Bid</Button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </main>
   );
 }
